aca10f65e5f10978d50dccc572370597a75af3d6
Casey-Gale <kcasey.gale@gmail.com>
2023-04-06 12:41:11 -0700

    v1.0
    deleted obsolete files, added error handling

--- assign1/assignment1.cpp
+++ assign1/assignment1.cpp
@@ -3,8 +3,8 @@
 #include <ctime>
 using namespace std;
 
-void add_marble(int& color, char c);
-char draw_marble(int& r, int& g, int& b);
+void add_marble(int& r, int& g, int& b);
+string draw_marble(int& r, int& g, int& b);
 int count_marbles(int r, int g, int b);
 
 int main() {
@@ -13,43 +13,93 @@ int main() {
     int g = 0;
     int b = 0;
 
+    do {
+        cout << "1-Add a marble" << endl;
+        cout << "2-Draw a marble" << endl;
+        cout << "3-Count marbles" << endl;
+        cout << "Anything else-End program" << endl;
+        cout << "Choice: ";
+        string choice;
+        getline(cin, choice);
+        cin.clear();
+        cout << endl;
+        string str;
+        if(choice.length() == 1) {
+            switch(choice[0]) {
+                case '1':
+                    add_marble(r, g, b);
+                    break;
+                case '2':
+                    str = draw_marble(r, g, b);
+                    if(str != "There aren't any marbles in the bag!") {
+                        cout << "You drew a " << str << " marble from the bag!" << endl;
+                    }
+                    else {
+                        cout << str << endl;
+                    }
+                    break;
+                case '3':
+                    cout << "There are " << count_marbles(r, g, b) << " marbles in the bag!" << endl;
+                    break;
+                default:
+                    return 0;
+            }
+        }
+        else {
+            break;
+        }
+    } while(true);
+
     return 0;
 }
 
-void add_marble(int& r, int& g, int& b, char c) {
-    if(c == 114) { //r
+//prompts a user for the color of a marble and increments that marble color
+void add_marble(int& r, int& g, int& b) {
+    cout << "Which color do you want to add?" << endl;
+    cout << "r-Add a red marble" << endl;
+    cout << "g-Add a green marble" << endl;
+    cout << "b-Add a blue marble" << endl;
+    cout << "Choice: ";
+    string c;
+    getline(cin, c);
+    cin.clear();
+    cout << endl;
+    if(c == "r") { //r
         r++;
     }
-    else if(c == 103) { //g
+    else if(c == "g") { //g
         g++;
     }
-    else if(c == 98) { //b
+    else if(c == "b") { //b
         b++;
+    }
+    else {
+        cout << "There is no color by that name!" << endl;
     }
 }
 
-char draw_marble(int& r, int& g, int& b) {
+//randomly draws a marble (that hopefully has the proper chances), returns the color, and decrements the marble color
+string draw_marble(int& r, int& g, int& b) {
     int n = count_marbles(r, g, b);
     if(!n) {
-        cout << "There aren't any marbles in the bag!" << endl;
-        return 'x';
+        return "There aren't any marbles in the bag!";
     }
     int prob = rand() % n + 1; //random number from 1 to number of marbles
     if(prob <= r) {
         r--;
-        return 'r';
+        return "red";
     }
-    else if(prob r + g) {
+    else if(prob <= r + g) {
         g--;
-        return 'g';
+        return "green";
     }
     else {
         b--;
-        return 'b';
+        return "blue";
     }
 }
 
 //adds the three integers together
-int count_marbles(int& r, int& g, int& b) {
+int count_marbles(int r, int g, int b) {
     return r + g + b;
 }
--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::cout << "Hello world" << std::endl;
     std::cout << "bruh" << std::endl;
+    std::cout << "surely this will work" << std::endl;
     
     return 0;
 }
