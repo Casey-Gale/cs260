6d5291f1b486922db711531c697b68e9676b32d8
Casey-Gale <kcasey.gale@gmail.com>
2023-06-07 11:03:36 -0700

    initial framework for mst
    added classes for auto-sorted list,
    
    + bare-bones mst code

--- assign8/assignment8.cpp
+++ assign8/assignment8.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 
 using std::cout;
 using std::endl;
@@ -15,6 +16,94 @@ struct edge {
     int weight;
 };
 
+// auto sorted list from assignment 5!
+class Node {
+    private:
+        
+    public:
+        edge* val;
+        Node* next;
+        Node();
+        Node(edge*);
+};
+
+Node::Node() {
+    this->val = nullptr;
+    this->next = nullptr;
+}
+Node::Node(edge* val) {
+    this->val = val;
+    this->next = nullptr;
+}
+
+class List {
+    private: 
+        Node* head;
+        int length;
+    public:
+        List();
+        int get_length() const;
+        void push(edge* val); //pushes to a sorted location in the list
+        edge* pop_front();
+};
+List::List() {
+    this->head = nullptr;
+    this->length = 0;
+}
+int List::get_length() const {return this->length;}
+
+//automatically pushes a value to a sorted list in a sorted position
+void List::push(edge* new_val) {
+    //cout << "Pushing " << new_val << endl;
+    //if list is empty
+    Node* current = this->head;
+    if(this->length == 0) {
+        //cout << "length == 0" << endl;
+        this->head = new Node(new_val); // new node->next is automatically nullptr
+    }
+    // checks if the value is less than the first 
+    else if(new_val->weight < this->head->val->weight) {
+        //cout << "current == head" << endl;
+        current = new Node(new_val);
+        current->next = this->head;
+        this->head = current;
+    }
+    //else
+    else {
+        while(current->next != nullptr && new_val->weight > current->next->val->weight) {
+            current = current->next;
+        }
+    
+        // checks if current is at the end of the list
+        if(current->next == nullptr) {
+            //cout << "current->next == nullptr" << endl;
+            current->next = new Node(new_val);
+        }
+        // now, we may assume that current is pointing to an object in the middle of the list
+        else {
+            //cout << "current is in the middle" << endl;
+            Node* after = current->next;
+            current->next = new Node(new_val);
+            current->next->next = after;
+        }
+    }
+    this->length++;
+}
+
+// pops out the front (smallest) value from the list
+// ASSUMES THE LIST CONTAINS SOMETHING
+edge* List::pop_front() {
+    edge* e = this->head->val;
+    Node* current = this->head->next;
+    delete this->head;
+    this->head = current;
+    return e;
+}
+
+
+
+
+
 class GraphNode {
     private: 
         string val;
@@ -24,7 +113,7 @@ class GraphNode {
         ~GraphNode();
         GraphNode(string new_val);
         void add_edge(GraphNode* destination, int weight);
-        string get_value();
+        string get_val();
         vector<edge*> get_neighbors();
 };
 
@@ -44,7 +133,7 @@ void GraphNode::add_edge(GraphNode* destination, int weight) {
     neighbors.push_back(new_edge);
     //okay!
 }
-string GraphNode::get_value() {return this->val;}
+string GraphNode::get_val() {return this->val;}
 vector<edge*> GraphNode::get_neighbors() {return neighbors;}
 
 class Graph {
@@ -53,6 +142,7 @@ class Graph {
         vector<GraphNode*> nodes;
     public:
         Graph();
+        Graph(GraphNode* new_node);
         ~Graph();
         // get size of graph nodes list
         int get_size();
@@ -63,11 +153,16 @@ class Graph {
         // shortest path, return string representing shortest paths from source to each destination node
         string shortest_path(string source_name);
         // minimum spanning tree
+        Graph* mst();
 };
 
 Graph::Graph() {
     // do nothing i guess
 }
+Graph::Graph(GraphNode* new_node) {
+    this->nodes.push_back(new_node);
+}
+
 Graph::~Graph() {
 
 }
@@ -94,15 +189,47 @@ void Graph::add_edge(string source_name, string destination_name, int weight) {
         }
     }
     // make edge between source and destination
-    if(source_node != nullptr && destination_name != nullptr) {
+    if(source_node != nullptr && destination_node != nullptr) {
         source_node->add_edge(destination_node, weight);
         destination_node->add_edge(source_node, weight);
     }
 
 }
 
-string shortest_path(string source_name) {
+string shortest_path(string source_name, string destination_name) {
+    return ":)";
+}
 
+Graph* Graph::mst() {
+    // create an ascending list of edges in the whole graph
+    // implemented using auto-sorted list from assignment 5
+    List edges;
+    vector<set<GraphNode*>> subsets;
+    for(GraphNode* node : this->nodes) {
+        for(edge* e : node->get_neighbors()) {
+            edges.push(e);
+        }
+        subsets.insert(node);
+    }
+    // create a vector of subsets containing a single node each ^^
+
+    // iterate through all edges (lowest weights first) and attempt to union sets
+    // if possible edge would create a cycle, remove it
+
+    for(edge* e : edges) {
+        for(auto subset : subsets) {
+            if(subset.count(e->source)) {
+                // if subset contains e->source, make note of subset (subset ptr maybe?)
+            }
+            if(subset.count(e->destination)) {
+                // if subset contains e->destination, make note of subset (subset ptr maybe?)
+            }
+            
+        }
+        // if e->source set == e->destination set, remove edge
+    }
+
+    return nullptr;
 }
 
 
@@ -113,11 +240,11 @@ int main() {
 
     // create a simple node to test :)
     GraphNode* new_node = new GraphNode("Corvallis"); // default constructor
-    cout << "new_node->get_value(): " << new_node->get_value() << endl << "new_node->get_neighbors().size(): " << new_node->get_neighbors().size() << endl << endl;
+    cout << "new_node->get_val(): " << new_node->get_val() << endl << "new_node->get_neighbors().size(): " << new_node->get_neighbors().size() << endl << endl;
 
     // add edge to node :)
     new_node->add_edge(new_node, 1);
-    cout << "new_node->get_value(): " << new_node->get_value() << endl << "new_node->get_neighbors().at(0)->destination->get_value(): " << new_node->get_neighbors().at(0)->destination->get_value() << endl << endl;
+    cout << "new_node->get_val(): " << new_node->get_val() << endl << "new_node->get_neighbors().at(0)->destination->get_val(): " << new_node->get_neighbors().at(0)->destination->get_val() << endl << endl;
 
     // create graph
 
